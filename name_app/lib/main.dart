284e5d6a6837e24ae00417c6ab6525180188f637
Bridget Torres <67389317+bridgettorres@users.noreply.github.com>
2024-09-06 17:56:38 -0500

    Added a new button, and fixed UI

--- name_app/lib/main.dart
+++ name_app/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true, 
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange), //5.button scheme
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey), //5.button scheme
         ),
         home: MyHomePage(), //6.the home page of the app is MyHomePage
       ),
@@ -26,9 +26,13 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier { //MyApp state defines the data the app needs to function, ChangeNotifier can notify others about its own changes, so if a current word pair changes some widgets in the app would need to know this
   var current = WordPair.random(); //it initializes the word randomly at start of the app
-
+  var randomname = WordPair.random();//initizaling the random name at start
   void getNext() { //reassigns current with a new random Word pair
     current = WordPair.random();
+    notifyListeners(); //peopel who listen to me must know about this change
+  }
+  void getNewName() { //reassigns random_name with a new random Word pair
+    randomname = WordPair.random();
     notifyListeners(); //peopel who listen to me must know about this change
   }
 } //wordpair comes from the package english words
@@ -41,18 +45,58 @@ class MyHomePage extends StatelessWidget {
     var appState = context.watch<MyAppState>(); //I want to rebuild everytime this app state gets updated or changed
     var pair = appState.current; 
     return Scaffold( 
-      body: Column( 
-        children: [
-          Text('Erick smells:'),
-        //  Text(appState.current.asLowerCase), //take the current that is on top and it renders it as lowercase
-          Text(pair.asLowerCase),
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            },
-            child: Text('Next'),
-          ),
-        ],
+      body: Center(
+        child: Column(  //By default, columns lump their children to the top, which is why the text is shown to be at the top of the app
+          mainAxisAlignment: MainAxisAlignment.center, //centers the children inside the column
+          children: [
+            Text('Cool Names:'),
+          //  Text(appState.current.asLowerCase), //take the current that is on top and it renders it as lowercase
+            BigCard(pair: pair),
+            SizedBox(height: 10), //adds space between card and button
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text('Next'),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: (){
+                appState.getNewName();
+              },
+              child: Text('Last Name'),
+            ),
+            Text('Cool Last Names: ${appState.randomname.asLowerCase}'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair; 
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context); //requests the app's current theme
+    final style = theme.textTheme.displayMedium!.copyWith( //(theme.textTheme) access the themes font, (.copywith) copies the text but with changes deifned, (!)assures Dart you know what you're doing
+      color: theme.colorScheme.onPrimary,
+    );
+    return Card(
+      color: theme.colorScheme.primary, //defines the card's color to be the same as the theme's colorScheme property, primary is the strongest color
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}", //this will allow screen readers to correctly pronounce each geneerated word 
+        ),
       ),
     );
   }
